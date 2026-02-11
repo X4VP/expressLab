d8abcc527661cc138fd2bbad8c89bbd6244e2ffb
X. Perez <xavierperez890@gmail.com>
2026-02-11 12:17:29 -0500

    created a server with user names stuff

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express(); //calling express function 
 
+app.set('view engine','ejs');
 app.get('/', (req, res) => {
     console.log('Here');
-    res.render('index');
+    res.render('index',{userName:'world'});
 });
 app.get('/potatoe', (req, res) => {
     res.send('<p> Here are your potatoes </p>');
 });
 
 app.get('/status', (req, res) => {
-    res.status(500).send('Hoi');
+    res.download('server.js');
+
 });
+
+app.get(/users', (req, res) => {
+    res.send('User List');
+});
+
+app.get('/users/new', (req, res) => {
+    res.send('New User Form');
 
 app.listen(3030);
